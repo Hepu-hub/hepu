0f25dcc6f426592262e21d23661e9bbd127bcfd2
hepu-pub <551514336@qq.com>
2025-02-24 22:06:02 +0800

    first

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a number:");
+        double number =double.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter a number:");
+        double number2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter a op:");
+        char op = char.Parse(Console.ReadLine());
+        double result=0.0;
+        switch (op)
+        {
+            case '+': result = number + number2;
+                break;
+            case '-': result = number - number2;
+                break;
+            case '*': result = number * number2;
+                break;
+            case '/': result = number / number2;
+                break;
+        }
+        Console.WriteLine("the result is");
+        Console.WriteLine(result);
+    }
+}
