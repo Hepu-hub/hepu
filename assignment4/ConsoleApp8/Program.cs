2300ab38c27bfebbdb687a7e6911a6631b7f734a
hepu-pub <551514336@qq.com>
2025-03-18 17:14:43 +0800

    work4

---
+++ assignment4/ConsoleApp8/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ConsoleApp8;
+
+class Program
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+        public Node (T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head =  null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> node = new Node<T>(t);
+            if (head == tail == null)
+            {
+                head=tail = node;
+            }
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+        }
+
+        public void MyForeach(Action<T> action)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                action(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("please input the number of nodes: ");
+        int number;
+        List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+        list.Add(6);
+        list.ForEach(x=>Console.WriteLine(x));
+        int min=int.MaxValue;
+        list.ForEach(x=>{if (x < min) min=x;});
+        Console.WriteLine(min);
+        int max=int.MinValue;
+        list.ForEach(x=>{if (x > max) max=x;});
+        Console.WriteLine(max);
+        int sum = 0;
+        list.ForEach(x=>sum += x);
+        Console.WriteLine(sum);
+    }
+}
---
+++ assignment4/ConsoleApp9/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleApp9;
+
+using System;
+using System.Threading;
+
+public delegate void OnclickCurrentTime(DateTime time);
+public delegate void OnclickAlarmtime(DateTime time);
+
+public class Alarm
+{
+    public DateTime AlarmTime { get; set; }
+    public DateTime CurrTime { get; set; }
+    public event OnclickCurrentTime OnclickCurrentTimeEvent;
+    public event OnclickAlarmtime OnclickAlarmtimeEvent;
+
+    public Alarm()
+    {
+        CurrTime = DateTime.Now;
+    }
+
+    public void StartAlarm()
+    {
+        Console.WriteLine("Alarm started");
+        while (true)
+        {
+            CurrTime = DateTime.Now;
+            OnclickCurrentTimeEvent(CurrTime);
+            if (AlarmTime <= DateTime.Now)
+            {
+                OnclickAlarmtimeEvent(CurrTime);
+                break;
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Alarm alarm = new Alarm();
+        alarm.OnclickAlarmtimeEvent += delegate(DateTime time)
+        {
+            Console.WriteLine($"alarm alarm at { time }");
+        };
+        alarm.OnclickCurrentTimeEvent += delegate(DateTime time)
+        {
+            Console.WriteLine($"OnclickCurrentTimeEvent {time}");
+        };
+        alarm.AlarmTime = DateTime.Now.AddSeconds(10);
+        alarm.StartAlarm();
+    }
+}
